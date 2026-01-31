e55c8ea070049234f8e52d933de5da71e4752672
Ashutosh-kumar-code <ashu9908kumar@gmail.com>
2026-01-31 21:53:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Docker + AWS working 🚀"
+  });
+});
+
+app.post("/test", (req, res) => {
+  const { name, email } = req.body;
+
+  if (!name || !email) {
+    return res.status(400).json({
+      success: false,
+      message: "name and email required"
+    });
+  }
+
+  res.json({
+    success: true,
+    data: { name, email }
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
